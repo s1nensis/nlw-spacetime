6c1bbf875d1fa2ef98039e5e4b32d945fe103ddd
Gustavo Jorge Nau <gustavo.22jn@gmail.com>
2023-05-20 16:19:31 -0300

    initial commit

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb-typescript/base",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "import"],
+  rules: {},
+};
